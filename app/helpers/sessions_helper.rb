85f9d5ea9af3365eeef24f15d0e0691cd34456e5
kakariko <kakariko01@gmail.com>
2021-05-04 20:07:48 +0000

    Finish ch10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,6 +16,11 @@ module SessionsHelper
       end
     end
   end
+  
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
+  end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
@@ -41,5 +46,16 @@ module SessionsHelper
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
+  end
+  
+  # 記憶したURL（もしくはデフォルト値）にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
